b7bdb97f8fc13bfd50b234facb85de4ed9de9105
Pavel Gasheev <p.gasheev@yandex.ru>
2023-02-13 17:35:35 +0300

    Add RTC calibration

--- src/rtc.c
+++ src/rtc.c
@@ -2,6 +2,28 @@
 #include "stm32g0xx.h"
 #include "gpio.h"
 
+#define CALIB_TIM               TIM17
+#define CALIB_TIM_CLOCK_EN()    do { RCC->APBENR2 |= RCC_APBENR2_TIM17EN; } while (0)
+
+enum meas_status {
+    MEAS_STOPPED,
+    MEAS_IN_PROGRESS,
+    MEAS_FINISHED,
+    MEAS_ERROR,
+};
+
+struct meas_ctx {
+    TIM_TypeDef *tim;
+    enum meas_status status;
+    bool pulse_captured;
+    uint16_t uif_cnt;
+    uint32_t period;
+    uint16_t start_crr;
+};
+
+struct meas_ctx ref_meas_ctx;
+struct meas_ctx rtc_meas_ctx;
+
 static const struct rtc_time init_time = {
     .seconds = 0x00,
     .minutes = 0x00,
@@ -12,10 +34,20 @@ static const struct rtc_time init_time = {
     .years = 0x20
 };
 
-static inline void start_init_disable_wpr(void)
+static inline void disable_wpr(void)
 {
     RTC->WPR = 0xCA;
     RTC->WPR = 0x53;
+}
+
+static inline void enable_wpr(void)
+{
+    RTC->WPR = 0;
+}
+
+static inline void start_init_disable_wpr(void)
+{
+    disable_wpr();
 
     RTC->ICSR |= RTC_ICSR_INIT;
     while ((RTC->ICSR & RTC_ICSR_INITF) == 0) {}
@@ -26,7 +58,16 @@ static inline void end_init_enable_wpr(void)
     RTC->ICSR &= ~RTC_ICSR_INIT;
     while (RTC->ICSR & RTC_ICSR_INITF) {}
 
-    RTC->WPR = 0;
+    enable_wpr();
+}
+
+static inline void reset_meas_context(struct meas_ctx *ctx)
+{
+    ctx->status = MEAS_STOPPED;
+    ctx->pulse_captured = 0;
+    ctx->uif_cnt = 0;
+    ctx->start_crr = 0;
+    ctx->period = 0;
 }
 
 static void set_datetime(const struct rtc_time * tm)
@@ -45,6 +86,9 @@ static void set_datetime(const struct rtc_time * tm)
 
 void rtc_init(void)
 {
+    reset_meas_context(&ref_meas_ctx);
+    reset_meas_context(&rtc_meas_ctx);
+
     // Set RTC output on PA4
     GPIO_SET_PUSHPULL(GPIOA, 4);
     GPIO_SET_OUTPUT(GPIOA, 4);
@@ -160,4 +204,122 @@ void rtc_set_alarm(const struct rtc_alarm * alarm)
         en;
 
     end_init_enable_wpr();
+}
+
+void rtc_start_calibration(void)
+{
+    reset_meas_context(&ref_meas_ctx);
+    reset_meas_context(&rtc_meas_ctx);
+
+    // Init PB9 GPIO
+    GPIO_SET_INPUT(GPIOB, 9);
+    GPIO_SET_AF(GPIOB, 9, 2);
+
+    CALIB_TIM_CLOCK_EN();
+
+
+    // Init CALIB_TIB
+    CALIB_TIM->CNT = 0;
+    CALIB_TIM->PSC = 8 - 1;
+    CALIB_TIM->ARR = 0xFFFF;
+    CALIB_TIM->CCMR1 |= TIM_CCMR1_CC1S_0 * 0b01;
+    CALIB_TIM->CCER |= TIM_CCER_CC1E;
+    CALIB_TIM->DIER |= TIM_DIER_UIE | TIM_CCER_CC1E;
+    TIM16->SR = 0;
+    NVIC_EnableIRQ(TIM17_IRQn);
+    NVIC_SetPriority(TIM17_IRQn, 0);
+
+    // Init TIM16 to capture wakeup events
+    RCC->APBENR2 |= RCC_APBENR2_TIM16EN;
+
+    TIM16->CNT = 0;
+    TIM16->PSC = 8 - 1;
+    TIM16->ARR = 0xFFFF;
+    TIM16->TISEL = 0b0011;      // RTC wakeup
+    TIM16->CCMR1 = TIM_CCMR1_CC1S_0 * 0b01;
+    TIM16->CCER |= TIM_CCER_CC1E;
+    TIM16->DIER |= TIM_DIER_UIE | TIM_DIER_CC1IE;
+    TIM16->SR = 0;
+    NVIC_EnableIRQ(TIM16_IRQn);
+    NVIC_SetPriority(TIM16_IRQn, 0);
+
+    // Enable RTC auto-wakeup signal with 1 s period
+    disable_wpr();
+
+    RTC->CR &= ~RTC_CR_WUTE;
+    while ((RTC->ICSR & RTC_ICSR_WUTWF) == 0) {}
+    // Set ck_wut wakeup clock
+    RTC->CR &= ~RTC_CR_WUCKSEL;
+    RTC->CR |= RTC_CR_WUCKSEL_0 * 0b011;    // RTC/2 clock is selected
+    RTC->WUTR = 32768 / 2 - 1;
+    RTC->CR |= RTC_CR_WUTE | RTC_CR_WUTIE;
+
+    enable_wpr();
+
+    RTC->SCR = RTC_SCR_CWUTF;
+
+    CALIB_TIM->CR1 |= TIM_CR1_CEN;
+    TIM16->CR1 |= TIM_CR1_CEN;
+}
+
+void TIM17_IRQHandler(void)
+{
+    if (CALIB_TIM->SR & TIM_SR_UIF) {
+        CALIB_TIM->SR = ~TIM_SR_UIF;
+        ref_meas_ctx.uif_cnt++;
+
+        // Check max events (no signal)
+    }
+
+    if (CALIB_TIM->SR & TIM_SR_CC1IF) {
+        CALIB_TIM->SR = ~TIM_SR_CC1IF;
+        // Reference pulse detected, skip first
+        uint16_t ccr = CALIB_TIM->CCR1;
+        if (!ref_meas_ctx.pulse_captured) {
+            ref_meas_ctx.pulse_captured = 1;
+            ref_meas_ctx.uif_cnt = 0;
+            ref_meas_ctx.start_crr = ccr;
+            rtc_meas_ctx.status = MEAS_FINISHED;
+        } else {
+            ref_meas_ctx.period = (65536 * ref_meas_ctx.uif_cnt + ccr) - ref_meas_ctx.start_crr;
+            ref_meas_ctx.status = MEAS_FINISHED;
+            //
+            RCC->APBRSTR2 |= RCC_APBRSTR2_TIM17RST;
+            RCC->APBRSTR2 &= ~RCC_APBRSTR2_TIM17RST;
+            RCC->APBENR2 &= ~RCC_APBENR2_TIM17EN;
+        }
+    }
+}
+
+void TIM16_IRQHandler(void)
+{
+    if (TIM16->SR & TIM_SR_UIF) {
+        TIM16->SR = ~TIM_SR_UIF;
+
+        rtc_meas_ctx.uif_cnt++;
+
+        // Check max events (no signal)
+    }
+
+    if (TIM16->SR & TIM_SR_CC1IF) {
+        TIM16->SR = ~TIM_SR_CC1IF;
+        // Reference pulse detected, skip first
+        uint16_t ccr = TIM16->CCR1;
+
+        // Clear RTC wakeup flag
+        RTC->SCR = RTC_SCR_CWUTF;
+
+        if (!rtc_meas_ctx.pulse_captured) {
+            rtc_meas_ctx.pulse_captured = 1;
+            rtc_meas_ctx.uif_cnt = 0;
+            rtc_meas_ctx.start_crr = ccr;
+        } else {
+            rtc_meas_ctx.period = (65536 * rtc_meas_ctx.uif_cnt + ccr) - rtc_meas_ctx.start_crr;
+            rtc_meas_ctx.status = MEAS_FINISHED;
+            //
+            RCC->APBRSTR2 |= RCC_APBRSTR2_TIM16RST;
+            RCC->APBRSTR2 &= ~RCC_APBRSTR2_TIM16RST;
+            RCC->APBENR2 &= ~RCC_APBENR2_TIM16EN;
+        }
+    }
 }
