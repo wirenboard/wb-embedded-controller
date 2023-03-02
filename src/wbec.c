68ac1e2fdbbd2b83d610e763b72732710e8ce1a5
Pavel Gasheev <p.gasheev@yandex.ru>
2023-03-02 16:55:23 +0300

    Add wbec

---
+++ src/wbec.c
@@ -0,0 +1,28 @@
+#include "config.h"
+#include "regmap.h"
+#include "pwrkey.h"
+#include "irq-subsystem.h"
+
+static const uint8_t fw_ver[] = { MODBUS_DEVICE_FW_VERSION_NUMBERS };
+
+void wbec_init(void)
+{
+    struct REGMAP_INFO wbec_info = {
+        .wbec_id = WBEC_ID,
+        .board_rev = 0x55,
+        .fw_ver_major = fw_ver[0],
+        .fw_ver_minor = fw_ver[1],
+        .fw_ver_patch = fw_ver[2],
+        .fw_ver_suffix = fw_ver[3],
+    };
+
+    regmap_set_region_data(REGMAP_REGION_INFO, &wbec_info, sizeof(wbec_info));
+}
+
+void wbec_do_periodic_work(void)
+{
+    if (pwrkey_handle_short_press()) {
+        irq_set_flag(IRQ_PWR_OFF_REQ);
+    }
+}
+
