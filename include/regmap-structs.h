87b765a2bb0077131ca9b559b705b30a2cdd703f
Pavel Gasheev <p.gasheev@yandex.ru>
2023-03-03 16:16:30 +0300

    Rework regmap define

--- include/regmap-structs.h
+++ include/regmap-structs.h
@@ -2,17 +2,9 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-enum regmap_rw {
-    REGMAP_RO,
-    REGMAP_RW
-};
-
 #define REGMAP(m) \
-    /* INFO: id and fw version */ \
-    m( \
-        INFO,       /* Region name */ \
-        0x00,       /* Region base address */ \
-        REGMAP_RO,  /* RO/RW */ \
+    /*  Name        Addr    RO/RW */ \
+    m(  INFO,       0x00,   RO, \
         /* Region data */ \
         uint8_t wbec_id; \
         uint8_t board_rev; \
@@ -21,11 +13,8 @@ enum regmap_rw {
         uint8_t fw_ver_patch; \
         int8_t fw_ver_suffix; \
     ) \
-    /* RTC Time */ \
-    m( \
-        RTC_TIME,   /* Region name */ \
-        0x10,       /* Region base address */ \
-        REGMAP_RW,  /* RO/RW */ \
+    /*  Name        Addr    RO/RW */ \
+    m(  RTC_TIME,   0x10,   RW, \
         /* Region data */ \
         uint8_t seconds; \
         uint8_t minutes; \
@@ -35,11 +24,8 @@ enum regmap_rw {
         uint8_t months; \
         uint8_t years; \
     ) \
-    /* RTC Alarm */ \
-    m( \
-        RTC_ALARM,  /* Region name */ \
-        0x20,       /* Region base address */ \
-        REGMAP_RW,  /* RO/RW */ \
+    /*  Name        Addr    RO/RW */ \
+    m(  RTC_ALARM,  0x20,   RW, \
         /* Region data */ \
         uint8_t seconds; \
         uint8_t minutes; \
@@ -47,19 +33,13 @@ enum regmap_rw {
         uint8_t days; \
         bool en:1; \
     ) \
-    /* RTC Config */ \
-    m( \
-        RTC_CFG,    /* Region name */ \
-        0x30,       /* Region base address */ \
-        REGMAP_RW,  /* RO/RW */ \
+    /*  Name        Addr    RO/RW */ \
+    m(  RTC_CFG,    0x30,   RW, \
         /* Region data */ \
         uint8_t offset; \
     ) \
-    /* ADC Data: voltages, temperature */ \
-    m( \
-        ADC_DATA,   /* Region name */ \
-        0x40,       /* Region base address */ \
-        REGMAP_RW,  /* RO/RW */ \
+    /*  Name        Addr    RO/RW */ \
+    m(  ADC_DATA,   0x40,   RW, \
         /* Region data */ \
         uint16_t v_in; \
         uint16_t v_bat; \
@@ -73,11 +53,8 @@ enum regmap_rw {
         uint16_t v_usb_debug_console; \
         uint16_t v_usb_debug_network; \
     ) \
-    /* ADC Config: UVP / OVP */ \
-    m( \
-        ADC_CFG,   /* Region name */ \
-        0x60,       /* Region base address */ \
-        REGMAP_RW,  /* RO/RW */ \
+    /*  Name        Addr    RO/RW */ \
+    m(  ADC_CFG,    0x60,   RW, \
         /* Region data */ \
         uint8_t lowpass_rc_a1; \
         uint8_t lowpass_rc_a2; \
@@ -88,11 +65,8 @@ enum regmap_rw {
         uint8_t v_out_uvp; \
         uint8_t v_out_ovp; \
     ) \
-    /* GPIO */ \
-    m( \
-        GPIO,       /* Region name */ \
-        0x80,       /* Region base address */ \
-        REGMAP_RW,  /* RO/RW */ \
+    /*  Name        Addr    RO/RW */ \
+    m(  GPIO,       0x80,   RW, \
         /* Region data */ \
         bool a1:1; \
         bool a2:1; \
@@ -100,45 +74,30 @@ enum regmap_rw {
         bool a4:1; \
         bool v_out:1; \
     ) \
-    /* Watchdog */ \
-    m( \
-        WDT,        /* Region name */ \
-        0x90,       /* Region base address */ \
-        REGMAP_RW,  /* RO/RW */ \
+    /*  Name        Addr    RO/RW */ \
+    m(  WDT,        0x90,   RW, \
         /* Region data */ \
         uint8_t timeout:4; \
         bool reset:1; \
         bool run:1; \
     ) \
-    /* Power control */ \
-    m( \
-        POWER_CTRL, /* Region name */ \
-        0xA0,       /* Region base address */ \
-        REGMAP_RW,  /* RO/RW */ \
+    /*  Name        Addr    RO/RW */ \
+    m(  POWER_CTRL, 0xA0,   RW, \
         /* Region data */ \
         bool off:1; \
     ) \
-    /* IRQ Flags */ \
-    m( \
-        IRQ_FLAGS,        /* Region name */ \
-        0xB0,       /* Region base address */ \
-        REGMAP_RO,  /* RO/RW */ \
+    /*  Name        Addr    RO/RW */ \
+    m(  IRQ_FLAGS,  0xB0,   RO, \
         /* Region data */ \
         uint8_t irqs; \
     ) \
-    /* IRQ Mask */ \
-    m( \
-        IRQ_MSK,    /* Region name */ \
-        0xB2,       /* Region base address */ \
-        REGMAP_RW,  /* RO/RW */ \
+    /*  Name        Addr    RO/RW */ \
+    m(  IRQ_MSK,    0xB2,   RW, \
         /* Region data */ \
         uint8_t irqs; \
     ) \
-    /* IRQ Clear */ \
-    m( \
-        IRQ_CLEAR,        /* Region name */ \
-        0xB4,       /* Region base address */ \
-        REGMAP_RW,  /* RO/RW */ \
+    /*  Name        Addr    RO/RW */ \
+    m(  IRQ_CLEAR,  0xB4,   RW, \
         /* Region data */ \
         uint8_t irqs; \
     ) \
