d884a129cd38c16623b5c4b807e271e389d966be
Pavel Gasheev <p.gasheev@yandex.ru>
2023-03-03 16:54:37 +0300

    Add wdt code

--- src/wdt.c
+++ src/wdt.c
@@ -1,5 +1,6 @@
 #include "wdt.h"
 #include "systick.h"
+#include "regmap.h"
 
 struct wdt_ctx {
     uint8_t timeout_s;
@@ -37,13 +38,27 @@ bool wdt_handle_timed_out(void)
 
 void wdt_do_periodic_work(void)
 {
-    if (!wdt_ctx.run) {
-        return;
+    if (wdt_ctx.run) {
+        systime_t wdt_time_ms = systick_get_time_since_timestamp(wdt_ctx.timestamp);
+        systime_t wdt_period_ms = wdt_ctx.timeout_s * 1000;
+        if (wdt_time_ms > wdt_period_ms) {
+            wdt_ctx.timed_out = 1;
+            wdt_start_reset();
+        }
     }
-    systime_t wdt_time_ms = systick_get_time_since_timestamp(wdt_ctx.timestamp);
-    systime_t wdt_period_ms = wdt_ctx.timeout_s * 1000;
-    if (wdt_time_ms > wdt_period_ms) {
-        wdt_ctx.timed_out = 1;
-        wdt_start_reset();
+
+    if (regmap_snapshot_is_region_changed(REGMAP_REGION_WDT)) {
+        struct REGMAP_WDT w;
+        regmap_get_snapshop_region_data(REGMAP_REGION_WDT, &w, sizeof(w));
+
+        wdt_set_timeout(w.timeout);
+        if (w.run && !wdt_ctx.run) {
+            wdt_start_reset();
+        } else if (!w.run) {
+            wdt_stop();
+        }
+        if (w.run && w.reset) {
+            wdt_start_reset();
+        }
     }
 }
