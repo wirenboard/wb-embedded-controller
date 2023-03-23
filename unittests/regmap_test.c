8a42d512d9cbe1df02a74bf9ab104257d299ec7c
Pavel Gasheev <p.gasheev@yandex.ru>
2023-03-23 15:20:47 +0300

    Rework regmap_test.c

--- unittests/regmap_test.c
+++ unittests/regmap_test.c
@@ -1,12 +1,12 @@
-#include "regmap.h"
+#include "regmap-int.h"
+#include "regmap-ext.h"
 #include <stdio.h>
 #include <errno.h>
 
-#define REGMAP_SIZE                                             sizeof(struct regmap)
+#define REGMAP_ADDRESS_MASK                                     (REGMAP_TOTAL_REGS_COUNT - 1)
 
 #define REGMAP_MEMBER(addr, name, rw, members)                  struct REGMAP_##name name;
 #define REGMAP_REGION_SIZE(addr, name, rw, members)             (sizeof(struct REGMAP_##name)),
-#define REGMAP_REGION_STRUCT_OFFSET(addr, name, rw, members)    (offsetof(struct regmap, name)),
 #define REGMAP_REGION_RW(addr, name, rw, members)               REGMAP_##rw,
 #define REGMAP_REGION_ADDR(addr, name, rw, members)             addr,
 
@@ -15,47 +15,57 @@ enum regmap_rw {
     REGMAP_RW
 };
 
-struct regmap {
-    REGMAP(REGMAP_MEMBER)
+// Описания регионов регистров
+struct regions_info {
+    uint16_t addr[REGMAP_REGION_COUNT];             // Адрес первого регистра в регионе
+    uint16_t size[REGMAP_REGION_COUNT];             // Размер региона в байтах
+    enum regmap_rw rw[REGMAP_REGION_COUNT];         // Тип региона RO/RW
 };
 
-
-static const struct regions_info {
-    uint8_t addr[REGMAP_REGION_COUNT];
-    uint8_t size[REGMAP_REGION_COUNT];
-    uint8_t struct_offset[REGMAP_REGION_COUNT];
-    bool rw[REGMAP_REGION_COUNT];
-} regions_info = {
+static const struct regions_info regions_info = {
     .addr = { REGMAP(REGMAP_REGION_ADDR) },
     .size = { REGMAP(REGMAP_REGION_SIZE) },
-    .struct_offset = { REGMAP(REGMAP_REGION_STRUCT_OFFSET) },
     .rw = { REGMAP(REGMAP_REGION_RW) },
 };
 
-
-void i2c_slave_set_busy(void)
+// Возвращает размер региона в байтах
+static inline uint16_t region_size(enum regmap_region r)
 {
-
+    return regions_info.size[r];
 }
 
-void i2c_slave_set_free(void)
+// Возвращает количество регистров в регионе
+static inline uint16_t region_reg_count(enum regmap_region r)
 {
-
+    return (region_size(r) + 1) / sizeof(uint16_t);
 }
 
+// Возвращает адрес первого регистра в регионе
+static inline uint16_t region_first_reg(enum regmap_region r)
+{
+    return regions_info.addr[r];
+}
+
+// Возвращает адрес последнего регистра в регионе
+static inline uint16_t region_last_reg(enum regmap_region r)
+{
+    return region_first_reg(r) + region_reg_count(r) - 1;
+}
 
 int main(void)
 {
+    const enum regmap_region last_region = REGMAP_REGION_COUNT - 1;
+
     // Check regions overlap
-    for (int r = 0; r < REGMAP_REGION_COUNT - 1; r++) {
-        if (regions_info.addr[r] + regions_info.size[r] > regions_info.addr[r + 1]) {
+    for (int r = 0; r < last_region - 1; r++) {
+        if ((region_first_reg(r) + region_reg_count(r)) > region_first_reg(r + 1)) {
             printf("ERROR: Region %d overlaps next region\n", r);
             return -EADDRINUSE;
         }
     }
 
     // Check last region
-    if (regions_info.addr[REGMAP_REGION_COUNT - 1] + regions_info.size[REGMAP_REGION_COUNT - 1] > (0xFF + 1)) {
+    if ((region_first_reg(last_region) + region_reg_count(last_region)) >= REGMAP_TOTAL_REGS_COUNT) {
         printf("ERROR: Last region overlaps max address 0xFF\n");
         return -EADDRINUSE;
     }
