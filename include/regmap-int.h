01507f2736076b7e594df387ebe1ed4b40598419
Pavel Gasheev <p.gasheev@yandex.ru>
2023-03-23 15:25:53 +0300

    Add regmap and SPI

---
+++ include/regmap-int.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include "regmap-structs.h"
+
+/**
+ * Работа с regmap изнутри прошивки
+ */
+
+#define __REGMAP_REGION_NAME(addr, name, rw, members)              REGMAP_REGION_##name,
+
+enum regmap_region {
+    REGMAP(__REGMAP_REGION_NAME)
+
+    REGMAP_REGION_COUNT
+};
+
+
+bool regmap_set_region_data(enum regmap_region r, const void * data, size_t size);
+void regmap_get_region_data(enum regmap_region r, void * data, size_t size);
+bool regmap_is_region_changed(enum regmap_region r);
+void regmap_clear_changed(enum regmap_region r);
