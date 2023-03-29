e867cf1e9f29101b47c3572000b51f26639b61be
Pavel Gasheev <p.gasheev@yandex.ru>
2023-03-29 11:36:41 +0300

    Rework pwrkey

--- src/pwrkey.c
+++ src/pwrkey.c
@@ -50,6 +50,52 @@ static inline bool get_pwrkey_state(void)
     #endif
 }
 
+// Выполняет подавление дребезга, результат записывает в gpio_ctx.logic_state
+static inline void debounce(bool gpio_state)
+{
+    // If GPIO state changed - save timestamp
+    if (gpio_ctx.prev_gpio_state != gpio_state) {
+        gpio_ctx.prev_gpio_state = gpio_state;
+        gpio_ctx.timestamp = systick_get_system_time_ms();
+    }
+
+    // If logic state and GPIO state differs - check debounce time elapsed
+    if (gpio_ctx.logic_state != gpio_state) {
+        systime_t held_time = systick_get_time_since_timestamp(gpio_ctx.timestamp);
+        if (held_time > PWRKEY_DEBOUNCE_MS) {
+            gpio_ctx.logic_state = gpio_state;
+        }
+    }
+}
+
+// Детектирует нажатия, результат - установленные флаги нажатий
+// logic_ctx.short_pressed_flag или logic_ctx.long_pressed_flag
+static inline void handle_presses(bool gpio_debounced_state)
+{
+    // Handle presses
+    if (logic_ctx.prev_logic_state != gpio_debounced_state) {
+        logic_ctx.prev_logic_state = gpio_debounced_state;
+        if (gpio_debounced_state) {
+            // If button pressed - save timestamp
+            logic_ctx.timestamp = systick_get_system_time_ms();
+            logic_ctx.long_pressed_detected = 0;
+        } else {
+            // If button released - check that it is not long press
+            if (!logic_ctx.long_pressed_detected) {
+                logic_ctx.short_pressed_flag = 1;
+            }
+        }
+    }
+
+    if (gpio_debounced_state) {
+        systime_t held_time = systick_get_time_since_timestamp(logic_ctx.timestamp);
+        if (held_time > PWRKEY_LONG_PRESS_TIME_MS) {
+            logic_ctx.long_pressed_flag = 1;
+            logic_ctx.long_pressed_detected = 1;
+        }
+    }
+}
+
 void pwrkey_init(void)
 {
     GPIO_S_SET_INPUT(pwrkey_gpio);
@@ -88,42 +134,8 @@ void pwrkey_do_periodic_work(void)
         return;
     }
 
-    // If GPIO state changed - save timestamp
-    if (gpio_ctx.prev_gpio_state != current_state) {
-        gpio_ctx.prev_gpio_state = current_state;
-        gpio_ctx.timestamp = systick_get_system_time_ms();
-    }
-
-    // If logic state and GPIO state differs - check debounce time elapsed
-    if (gpio_ctx.logic_state != current_state) {
-        systime_t held_time = systick_get_time_since_timestamp(gpio_ctx.timestamp);
-        if (held_time > PWRKEY_DEBOUNCE_MS) {
-            gpio_ctx.logic_state = current_state;
-        }
-    }
-
-    // Handle presses
-    if (logic_ctx.prev_logic_state != gpio_ctx.logic_state) {
-        logic_ctx.prev_logic_state = gpio_ctx.logic_state;
-        if (gpio_ctx.logic_state) {
-            // If button pressed - save timestamp
-            logic_ctx.timestamp = systick_get_system_time_ms();
-            logic_ctx.long_pressed_detected = 0;
-        } else {
-            // If button released - check that it is not long press
-            if (!logic_ctx.long_pressed_detected) {
-                logic_ctx.short_pressed_flag = 1;
-            }
-        }
-    }
-
-    if (gpio_ctx.logic_state) {
-        systime_t held_time = systick_get_time_since_timestamp(logic_ctx.timestamp);
-        if (held_time > PWRKEY_LONG_PRESS_TIME_MS) {
-            logic_ctx.long_pressed_flag = 1;
-            logic_ctx.long_pressed_detected = 1;
-        }
-    }
+    debounce(current_state);
+    handle_presses(gpio_ctx.logic_state);
 }
 
 bool pwrkey_handle_short_press(void)
