1e964054abe210a2ed85b27645a1a4aceb6cf2b7
Pavel Gasheev <p.gasheev@yandex.ru>
2023-03-31 10:27:43 +0300

    Rework USART, add comments

--- src/usart_tx.c
+++ src/usart_tx.c
@@ -2,6 +2,14 @@
 #include "gpio.h"
 #include "config.h"
 
+/**
+ * Модуль позволяет передавать строки в отладочный UART.
+ * Используется блокирующая передача.
+ * Возможно передавать как null-terminated строки, так и явновно указывать размер
+ */
+
+static const gpio_pin_t usart_tx_gpio = { EC_DEBUG_USART_GPIO };
+
 static inline void usart_transmit_char(char c)
 {
     while ((USART1->ISR & USART_ISR_TXE_TXFNF) == 0) {};
@@ -17,13 +25,16 @@ static inline void usart_wait_tranmission_complete(void)
 void usart_init(void)
 {
     // Init GPIO
-    GPIO_SET_OUTPUT(GPIOA, 9);
-    GPIO_SET_AF(GPIOA, 9, 1);
+    GPIO_S_SET_OUTPUT(usart_tx_gpio);
+    GPIO_S_SET_AF(usart_tx_gpio, EC_DEBUG_USART_GPIO_AF);
 
-    // Init USART1
-    RCC->APBENR2 |= RCC_APBENR2_USART1EN;
-    USART1->BRR = F_CPU / 115200;
-    USART1->CR1 |= USART_CR1_TE | USART_CR1_UE;
+    #ifdef EC_USE_USART1_DEBUG_TX
+        RCC->APBENR2 |= RCC_APBENR2_USART1EN;
+        USART1->BRR = F_CPU / EC_DEBUG_USART_BAUDRATE;
+        USART1->CR1 |= USART_CR1_TE | USART_CR1_UE;
+    #else
+        #error "Not supported USART"
+    #endif
 }
 
 void usart_tx_str_blocking(const char str[], size_t size)
