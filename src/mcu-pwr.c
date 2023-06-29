c6cdafc7f31828f58a2d6e5ac635c2eb40c855ce
Pavel Gasheev <p.gasheev@yandex.ru>
2023-06-29 17:01:27 +0300

    Rework function

--- src/mcu-pwr.c
+++ src/mcu-pwr.c
@@ -5,26 +5,23 @@
 
 enum mcu_poweron_reason mcu_get_poweron_reason(void)
 {
-    enum mcu_poweron_reason reason;
     if (PWR->SR1 & PWR_SR1_SBF) {
         PWR->SCR = PWR_SCR_CSBF;
         if (PWR->SR1 & (1 << (EC_GPIO_PWRKEY_WKUP_NUM + PWR_SR1_WUF1_Pos))) {
             PWR->SCR = (1 << (EC_GPIO_PWRKEY_WKUP_NUM + PWR_SCR_CWUF1));
-            reason = MCU_POWERON_REASON_POWER_KEY;
+            return MCU_POWERON_REASON_POWER_KEY;
         } else if (PWR_SR1_WUFI) {
             PWR->SCR = PWR_SR1_WUFI;
             if (RTC->SR & RTC_SR_WUTF) {
-                reason = MCU_POWERON_REASON_RTC_PERIODIC_WAKEUP;
+                return MCU_POWERON_REASON_RTC_PERIODIC_WAKEUP;
             } else {
-                reason = MCU_POWERON_REASON_RTC_ALARM;
+                return MCU_POWERON_REASON_RTC_ALARM;
             }
-        } else {
-            reason = MCU_POWERON_REASON_UNKNOWN;
         }
     } else {
-        reason = MCU_POWERON_REASON_POWER_ON;
+        return MCU_POWERON_REASON_POWER_ON;
     }
-    return reason;
+    return MCU_POWERON_REASON_UNKNOWN;
 }
 
 void mcu_goto_standby(uint16_t wakeup_after_s)
