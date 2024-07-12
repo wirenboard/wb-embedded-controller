dc090a8ece8a426464aa4471435f568d61e8cc28
Pavel Gasheev <p.gasheev@yandex.ru>
2024-07-12 10:26:28 +0300

    fix regmap atomic: merge is_changed and get_data functions

--- include/regmap-int.h
+++ include/regmap-int.h
@@ -19,6 +19,4 @@ enum regmap_region {
 
 void regmap_init(void);
 bool regmap_set_region_data(enum regmap_region r, const void * data, size_t size);
-void regmap_get_region_data(enum regmap_region r, void * data, size_t size);
-bool regmap_is_region_changed(enum regmap_region r);
-void regmap_clear_changed(enum regmap_region r);
+bool regmap_is_region_changed(enum regmap_region r, void * data, size_t size);
