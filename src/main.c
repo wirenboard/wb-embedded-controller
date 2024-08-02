3c7bc349545d07dcba52c041e2a3ee15ec27972c
Pavel Gasheev <p.gasheev@yandex.ru>
2024-08-02 11:00:29 +0300

    check hwrev

--- src/main.c
+++ src/main.c
@@ -47,7 +47,23 @@ int main(void)
     adc_init(ADC_CLOCK_NO_DIV, ADC_VREF_INT);
     while (!adc_get_ready()) {};
 
+    // Прежде чем инициализировать всё остальное, нужно проверить совместимость железа и прошивки
     hwrev_init();
+    if (hwrev_get() != WBEC_HWREV) {
+        // Прошивка не совместима с железом
+        // Дальше что-то делать смысла нет, т.к. мы не знаем что за железо и какие gpio чем управляют
+        // Инициализируем только системный светодиод, spi и regmap
+        // Чтобы из линукса можно было прочитать код железа и понять какую прошивку заливать
+        rcc_set_hsi_pll_64mhz_clock();
+        spi_slave_init();
+        regmap_init();
+        hwrev_put_hw_info_to_regmap();
+        // Редко мигаем светодиодом, чтобы понять что прошивка не совместима
+        system_led_blink(100, 2900);
+        while (1) {
+            system_led_do_periodic_work();
+        }
+    }
 
     // Первым инициализируется WBEC, т.к. он в начале проверяет причину включения
     // и может заснуть обратно, если решит.
@@ -64,6 +80,8 @@ int main(void)
     spi_slave_init();
     regmap_init();
     usart_init();
+
+    hwrev_put_hw_info_to_regmap();
 
     // Init subsystems
     irq_init();
