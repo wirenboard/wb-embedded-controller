586eae754361151ae08266e1ee8e11fb99e79135
Pavel Gasheev <p.gasheev@yandex.ru>
2024-08-19 11:42:51 +0300

    Add WBMZ charging control

--- src/wbmz-subsystem.c
+++ src/wbmz-subsystem.c
@@ -9,6 +9,11 @@ void wbmz_subsystem_do_periodic_work(void)
     p.powered_from_wbmz = wbmz_is_powered_from_wbmz();
     p.wbmz_stepup_enabled = wbmz_is_stepup_enabled();
 
+    #if defined WBEC_GPIO_WBMZ_CHARGE_ENABLE
+
+        p.wbmz_charging_enabled = wbmz_is_charging_enabled();
+
+    #endif
 
     regmap_set_region_data(REGMAP_REGION_PWR_STATUS, &p, sizeof(p));
 }
