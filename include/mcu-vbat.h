a0ced719b7d5182ec184901a378e80308eaa5e48
Nikolay Oleinik <nikolay.oleinik@wirenboard.com>
2026-04-30 17:59:55 +0700

    fix

--- include/mcu-vbat.h
+++ include/mcu-vbat.h
@@ -3,8 +3,7 @@
 void mcu_vbat_init(void);
 void mcu_vbat_check_do_periodic_work(void);
 
-// Триггерит внеочередное измерение Vbat из состояния IDLE.
-// Если сейчас идёт зарядка (CHARGING) - вызов игнорируется.
+// Триггерит внеочередное измерение Vbat
 void mcu_vbat_trigger_measurement(void);
 
 // Перезапускает алгоритм зарядки: включает PWR_CR4_VBE и сбрасывает
