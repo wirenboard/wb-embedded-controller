04592fd0f4a5079005e883fde7a71f6719680184
Pavel Gasheev <pavel.gasheev@wirenboard.com>
2026-08-13 16:19:40 +0300

    Revert "add internal vbat charging (#86)" (#96)
    This reverts commit b8608df4eba7ff461e06b51ad9fdb155ac389e6f.
    
    The VBAT charging algorithm does not work as needed, so the feature is
    rolled back and will be reworked separately.
    
    Two parts of the reverted commit are intentionally kept:
      * the stack_size.ld fix in system/build_common.mk, which is an
        independent build-system fix unrelated to charging;
      * the debian/changelog entries of the already released versions.
    
    The regmap loses the VBAT_STATUS region (0xE0) and the vbat_meas_en and
    vbat_charge_en bits in TEST (0xF0).

--- src/main.c
+++ src/main.c
@@ -25,7 +25,6 @@
 #include "software_i2c.h"
 #include "uart-regmap-subsystem.h"
 #include "wdt-stm32.h"
-#include "mcu-vbat.h"
 
 int main(void)
 {
@@ -82,8 +81,6 @@ int main(void)
     systick_init();
     adc_init(ADC_CLOCK_DIV_64, ADC_VREF_INT);
 
-    mcu_vbat_init();
-
     // Init drivers
     gpio_init();
     temperature_control_init();
@@ -126,8 +123,6 @@ int main(void)
         // Main algorithm
         linux_cpu_pwr_seq_do_periodic_work();
         wbec_do_periodic_work();
-
-        mcu_vbat_check_do_periodic_work();
 
         watchdog_reload();
     }
